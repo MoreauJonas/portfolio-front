7b865686c5f43a95073c356148d55239c49fb11e
Jonas Moreau <jonas.moreau@hotmail.com>
2022-11-11 17:34:36 +0100

    new navbar + responsive

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "../src/styles/index.scss";
 import App from "./App";
+import "../src/styles/index.scss";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
